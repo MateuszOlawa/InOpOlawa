6485ebc355261b5b3e868a7052c5a19ffb00866f
MateuszOlawa <Olawa.Mateusz@gmail.com>
2018-05-13 12:01:19 +0200

    Kalkulator

--- Kalkulator.py
+++ Kalkulator.py
@@ -1,12 +1,6 @@
-import math
-liczby = input()
-cyfry = liczby.split()
-n=0
-liczba1 = int(cyfry[0])
-liczba2 = int(cyfry[1])
-liczba3 = int(cyfry[0]) - int(cyfry[1])
-liczba1=math.factorial(liczba1)
-liczba2=math.factorial(liczba2)
-liczba3=math.factorial(liczba3)
-wynik = liczba1/(liczba2*liczba3)
-print(round(wynik))
+def dodaj():
+	a = input()
+	b = input()
+	print (int(a)+int(b))
+	
+dodaj()	
